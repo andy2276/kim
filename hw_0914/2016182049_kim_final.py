e333e14faf92403781c4a7b1124b77bb5bf043fb
andy2276 <42955340+andy2276@users.noreply.github.com>
2018-09-18 13:38:02 +0900

    final

---
+++ hw_0914/2016182049_kim_final.py
@@ -0,0 +1,72 @@
+from pico2d import *
+import os
+import math
+
+os.getcwd()
+
+os.chdir('..')
+os.chdir('resource')
+os.listdir() #이거 안해주면 읽지를 못함!
+
+open_canvas()
+
+grass = load_image('grass.png')
+runCharacer = load_image('run_animation.png')
+
+count = 0
+x = 0
+y = 90
+ho = 0
+speed = 0.01
+
+r = 250
+start_y = start_x = 300
+
+frame = 0
+while(count != 4):
+    clear_canvas()
+    grass.draw(400,30) #grass draw!!
+    runCharacer.clip_draw(frame * 100,0,100,100,x,y)
+    
+    frame = (frame+1)%8
+    update_canvas()
+    if(count == 0):
+        x +=5
+        if(x >=770):
+            count = 1
+    elif(count == 1):
+        y+=5
+        if(y>=570):
+            count=2
+    elif(count == 2):
+        x -=5
+        if(x<=20):
+            count = 3
+    elif(count == 3):
+        y -=5
+        if(y<=90):
+            count =4
+    
+    delay(speed)#speed!!! 1 slow 0.01 faster
+    get_events()
+x = 0
+y = 0
+
+frame = 0
+while(ho != 360):
+    clear_canvas()
+    grass.draw(400,30)
+    runCharacer.clip_draw(frame * 100,0,100,100,x,y)
+    ho +=5
+    y = math.sin(math.radians(ho))* r + start_y
+    x = math.cos(math.radians(ho))* r + start_x
+    frame = (frame+1)%8
+    update_canvas()
+    if(ho == 360):
+        ho = 0
+    delay(speed)
+    get_events()
+
+
+close_canvas()
+
