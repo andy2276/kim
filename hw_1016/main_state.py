4dab402f8e3e453cec1f135534da1ef7b8a548aa
김지민 <wlals2276@naver.com>
2018-10-18 02:24:30 +0900

    aaaaa

---
+++ hw_1016/main_state.py
@@ -0,0 +1,153 @@
+#import main module pico2d
+from pico2d import *
+#define import module
+import sys
+sys.path.insert(0,'..//hw_0928')
+import game_framework as gf
+import title_state
+import json
+import random as rd
+#module var initialization
+boys = None
+grass = None
+wp = None
+waypoints = None
+running = True
+count = 0
+
+#딕셔너리 멤버 함수 keys(),values(),items()모두 구성 요소를 보여주는 함수
+#
+action = {"LEFT_RUN": 0,"RIGHT_RUN":1,"LEFT_STAND":2,"RIGHT_STAND":3}
+
+#define class
+class clBoy():
+    ani = None
+    wp = None
+    def __init__(self):
+        self.name = 'no'
+        self.x = rd.randint(0,200)
+        self.y = rd.randint(90,550)
+        self.speed = rd.randint(1,3)
+        self.frame = rd.randint(0,7)
+        self.waypoints = []
+        self.state = action["LEFT_STAND"]
+        self.go = 1
+        if clBoy.ani == None:
+            clBoy.ani = load_image('../resource/animation_sheet.png')
+        if clBoy.wp == None:
+            clBoy.wp = load_image('../resource/wp.png')
+
+    def draw(self):
+        for wp in self.waypoints:
+            self.wp.draw(wp[0], wp[1])
+        self.ani.clip_draw(self.frame*100,self.state*100,100,100,self.x,self.y)
+        
+    def update(self):
+        self.frame = (self.frame + 1)%8
+        if len(self.waypoints)>0:
+            xp,yp = self.waypoints[0]
+            xd = xp - self.x
+            yd = yp - self.y
+            dist = math.sqrt(xd**2+ yd **2)
+            if dist > 0:
+                self.x += self.speed * xd/dist
+                self.y += self.speed * yd/dist
+                if self.x < xp :
+                    self.state = action["RIGHT_RUN"]
+                    self.go =action["RIGHT_RUN"]
+                if self.x > xp:
+                    self.state = action["LEFT_RUN"]
+                    self.go = action["LEFT_RUN"]
+
+                if xd < 0 and self.x< xp:
+                    self.x = xp
+                if xd > 0 and self.x> xp:
+                    self.x = xp
+                if yd < 0 and self.y< yp:
+                    self.y = yp
+                if yd > 0 and self.y> yp:
+                    self.y = yp
+                    
+                if(xp,yp)==(self.x,self.y):
+                    if self.go == action["RIGHT_RUN"]:
+                        self.state =action["RIGHT_STAND"]
+                    elif self.go == action["LEFT_RUN"] :
+                        self.state = action["LEFT_STAND"]
+                    del self.waypoints[0]
+            
+        
+class clGrass():
+    def __init__(self):
+        self.image = load_image('../resource/grass.png')
+    def draw(self,xa,ya): # class에는 무조건 self를 인자로 주어야한다!
+        self.image.draw(xa,ya)
+
+#start game_framework
+def handle_events():
+    global running
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            gf.quit()
+            running = False
+        elif e.type == SDL_KEYDOWN:
+            if e.key == SDLK_ESCAPE:
+                running = False
+                gf.change_state(title_state)
+        elif e.type == SDL_MOUSEBUTTONDOWN:
+            if e.button == 1:
+                for b in boys:
+                    xp = e.x 
+                    yp = 600 - e.y
+                    b.waypoints +=[(xp,yp)]
+            else:
+                for b in boys:
+                    b.waypoints = []
+                    if b.go == action["RIGHT_RUN"]:
+                        b.state = action["RIGHT_STAND"]
+                    if b.go == action["LEFT_RUN"]:
+                        b.state = action["LEFT_STAND"]
+num = 5
+
+
+
+def enter():
+    global boys,grass,num
+    open_canvas()
+    team_data_file = open('player.json','r')
+    team_data = json.load(team_data_file)
+    
+    boys = []
+    for i in team_data['boys']:
+        b = clBoy()
+        b.name = i['name']
+        b.state = action[i['StartState']]
+        b.x = i['x']
+        b.y = i['y']
+        boys.append(b)
+    
+    grass = clGrass()
+    team_data_file.close()
+def exit():
+    global boys, grass
+    del(grass)
+    del(boys)
+def update():
+    for b in boys:
+         b.update()
+def draw():
+    clear_canvas()
+    grass.draw(400,90)
+    for b in boys:
+        b.draw()
+    update_canvas()
+    
+if __name__ == '__main__':
+	import sys
+	glCurrentModule = sys.modules[__name__]	
+	open_canvas()
+	gf.run(glCurrentModule)
+	close_canvas()
+
+    
+    
--- term/script/readme.txt
+++ term/script/readme.txt
@@ -5,8 +5,14 @@
 	편할것이다.
 아군 클래스클래스
 	class clPlayerBody:
-		mvX-맴버변수 x 바디의 x위치
+		mvX-맴버변수 x 바디의 x위치 추가로 벡터좌표 mvVX와 mvVY를 만들어서 이동좌표를 만들자
 		mvY-멤버변수 y 바디의 y위치
+<<<<<<< HEAD
+		위에꺼는 절대 좌표를 받는 친구고 벡터좌표도 만들어서 간단하게 계산하자
+		mvVX,mvVY - 멤버변수 벡터 좌표 - 아래로 간다면 현재좌표에서 +(-mvVY를 해주면 된다)
+=======
+		현 mvx,mvy에다가 mvVx를 더하는 방식으로 하면 더 간단하게 계산이 될것이다.
+>>>>>>> 0d045d0357603cfffdaa18594645f15851c72e77
 		mvHp-멤버변수 hp
 		mvSpeed-멤버변수 speed
 		mvBodyImage - 멤버변수 bodyimage
@@ -20,3 +26,7 @@
 2.교수님의 수정된 gameframe work를 쓸것
 3.로고 이후에 무조건 로딩스테이트를 들어갈 것
 4.시작은 무조건 main스테이트고 그다음 로고 스테이트고 그다음 로딩 스테이트로 진입
+[추가]
+1.https://m.blog.naver.com/samsjang/220498694383 openCV공부
+2.rotate 함수 구현
+3.타임 모듈 추가하여 프레임관련된 것을 timeToDeltaTime으로 만들기
