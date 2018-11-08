8eb0eea2cda0e0b4bc80dc2694812fa91a974afd
김지민 <wlals2276@naver.com>
2018-11-09 00:01:31 +0900

    siva

--- hw_1106/boy.py
+++ hw_1106/boy.py
@@ -1,6 +1,8 @@
 # import main module pico2d
 from pico2d import *
 # define import module
+import math
+import random
 import sys
 
 #sys.path.insert(0, '..//hw_0928')
@@ -14,6 +16,10 @@ wp = None
 waypoints = None
 running = True
 count = 0
+FireBall = []
+
+GRAVITY = 10 / 33
+BOUNCING_GROUND = 62
 
 # 딕셔너리 멤버 함수 keys(),values(),items()모두 구성 요소를 보여주는 함수
 
@@ -29,6 +35,53 @@ key_event_table = {
 }
 
 # define class
+def grave(speed,rad):
+    v = speed
+    d = rad
+
+    ti = v * math.sin(d*math.pi)/9.8
+    hi = (v**2)*(math.sin(d*math.pi))**2/(2*9.8)
+    di = (v**2)*(math.sin(d*math.pi))/9.8
+
+    return (ti,hi,di)
+
+class clBall:
+    image = None
+    def __init__(self,x,y,mx,my):
+        self.x,self.y = x,y+20
+        self.mx,self.my = mx,my
+
+        self.rad = math.atan2(self.my - self.y, self.mx - self.x)
+        self.grav = 0.01
+        self.time = 1/60
+        #self.dist = (math.sqrt((self.mx - self.x) ** 2 + (self.my - self.y) ** 2)) * 0.1
+        self.dist = 0.001
+        self.sto = False
+        self.dx = (self.dist / self.time) * math.cos(self.rad)
+        self.dy = (self.dist / self.time) + math.sin(self.rad) - (self.grav * self.time)
+        print(self.dx,self.dy)
+
+
+
+
+        if clBall.image == None:
+            clBall.image = load_image('../resource/ball21x21.png')
+    def draw(self):
+        self.image.draw(self.x,self.y)
+    def update(self):
+        #print(self.dist)
+        #self.dx = math.cos(self.rad)
+        #self.dy = ((0.0000001 * math.sin(self.rad))-self.grav*1.0*self.time)*self.time
+        if self.sto != True:
+            self.x += self.dx * self.time
+            self.y += self.dy-(self.grav*(self.time**2))/2
+            self.time += 1/60
+
+    def stop(self,a):
+        if a == 1:
+            self.x,self.y,self.dx,self.dy = 10,20,0,0
+
+
 class clBoy():
     ani = None
     pointer = None
@@ -43,6 +96,7 @@ class clBoy():
         self.speed = 1
         self.frame = 0
         self.state = 3
+        self.Fire = []
         if clBoy.ani == None:
             clBoy.ani = load_image('../resource/animation_sheet.png')
             clBoy.pointer = load_image('../resource/pointer.png')
@@ -63,6 +117,7 @@ class clBoy():
             elif self.x > 700:
                 self.x -= 10
     def handle_events(self,e):
+        global FireBall
         if(e.type,e.key) in key_event_table:
             key_event = key_event_table[(e.type, e.key)]
             if key_event == RIGHT_DOWN:
@@ -81,6 +136,8 @@ class clBoy():
                 self.state = 2
                 self.dx += self.speed
                 if self.dx > 0: self.dir = 1
+            elif key_event == SPACE_DOWN:
+                FireBall += [clBall(self.x, self.y, self.mx,self.my)]
 
 
 
@@ -95,7 +152,7 @@ class clGrass():
 
 # start game_framework
 def handle_events():
-    global running,boy
+    global running,boy,FireBall
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT:
@@ -113,21 +170,29 @@ def enter():
     grass = clGrass()
 
 def exit():
-    global boy, grass
+    global boy, grass,FireBall
     del (grass)
     del (boy)
+    del (FireBall)
 
 def update():
-    global boy
+    global boy,FireBall
 
     boy.update()
-
+    for f in FireBall:
+        if (f.x > get_canvas_width() or f.x < 0) and (f.y > get_canvas_height() or f.y <0):
+            f.stop(1)
+            print("del!!")
+        else:
+            f.update()
 
 def draw():
-    global boy
+    global boy, FireBall
     clear_canvas()
     grass.draw(400, 90)
-    boy.draw();
+    boy.draw()
+    for f in FireBall:
+        f.draw()
     update_canvas()
 
 
