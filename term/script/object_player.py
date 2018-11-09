202821b1447a50cc440ba787620fdd7903f1e05e
김지민 <wlals2276@naver.com>
2018-11-10 02:11:50 +0900

    ha...
    파일을 정리하자

---
+++ term/script/object_player.py
@@ -0,0 +1,112 @@
+from pico2d import *
+
+import math
+
+MOVE_TIME = 1/60
+#-------Tank Body---------
+class Body:
+    KeyEvent = [SDLK_a, SDLK_d, SDLK_w, SDLK_s]
+    image = None
+    def __init__(self,px,py,prad,prs,pfs,pbs):
+        self.x ,self.y =px,py
+
+        self.rotSpeed= prs/10 * math.pi/60
+        self.fwSpeed = pfs*MOVE_TIME
+        self.bkSpeed = pbs*MOVE_TIME
+        print(self.fwSpeed,self.bkSpeed)
+        self.rad = prad
+        self.key = {}
+        for k in Body.KeyEvent :
+            self.key[k] = False
+        if Body.image ==None : Body.image = load_image("../res/object/character/player_body_pix2.png")
+
+    def draw(self):
+        self.image.composite_draw(self.rad, "" , self.x, self.y)
+
+    def update(self):
+        rot = 1 if self.key[SDLK_a] else 0
+        rot += -1 if self.key[SDLK_d] else 0
+        mov = 1 if self.key[SDLK_w] else 0
+        mov += -1 if self.key[SDLK_s] else 0
+        if rot != 0:
+            if mov < 0:
+                rot = -rot
+            self.rad += rot*self.rotSpeed
+        #rot and mov not over 1 or -1
+        if mov !=0:
+            self.x += mov * self.fwSpeed * math.cos(self.rad)
+            self.y += mov * self.fwSpeed * math.sin(self.rad)
+
+        return self.x, self.y
+
+    def handle_event(self,keys):
+        if keys.type == SDL_KEYDOWN or keys.type == SDL_KEYUP:
+            if keys.key in Body.KeyEvent:
+                self.key[keys.key] = keys.type == SDL_KEYDOWN
+
+#-------Tank Barrel---------
+class Barrel:
+    image = None
+    def __init__(self,px,py,prs):
+        self.x,self.y = px,py
+
+        self.mx,self.my = 0, 0
+        self.rad = 0.0
+        self.rotSpeed = prs/10 * math.pi/60
+        if Barrel.image == None: Barrel.image = load_image('../res/object/character/player_barrel_pix2.png')
+
+    def draw(self):
+        self.image.composite_draw(self.rad,"",self.x,self.y)
+        self.rad = math.atan2(self.y - self.my,self.x - self.mx)
+
+    def update(self, px, py):
+
+        #print(self.rad)
+        self.x , self.y = px, py
+
+
+    def handle_event(self,keys):
+        if keys.type == SDL_MOUSEMOTION:
+            print(keys.x,600- keys.y)
+            self.mx, self.my = keys.x, 600 - keys.y
+            print(self.mx, self.my)
+
+#-------Tank Body+Barrel---------
+class Player:
+    def __init__(self, sName, sX, sY, sHp, sRad, sPrs, sPfs, sPbs):
+        self.id = sName
+        self.x, self.y = sX, sY
+
+        self.x = get_canvas_width() // 2
+        self.y = get_canvas_height() // 2
+
+        self.hp = sHp
+        self.rad = math.pi/(180/sRad)
+        self.rotSpeed = sPrs
+        self.bodyFwSpeed = sPfs
+        self.bodyBkSpeed = sPbs
+
+        self.body = Body(self.x,self.y,self.rad,self.rotSpeed,self.bodyFwSpeed,self.bodyBkSpeed)
+        self.barrel = Barrel(self.x,self.y,self.rotSpeed)
+
+
+        self.collision = 0  # 함수만들거임
+
+    def draw(self):
+        self.body.draw()
+        self.barrel.draw()
+    def update(self):
+        self.x, self.y = self.body.update()
+
+        self.barrel.update(self.x, self.y)
+
+    def handle_event(self,keys):
+
+        self.body.handle_event(keys)
+        self.barrel.handle_event(keys)
+
+
+
+
+
+
---
+++ term/script/player_object_control.py
@@ -0,0 +1,54 @@
+from pico2d import *
+
+import game_framework as gf
+import json
+import object_player
+
+
+
+def enter():
+    global player
+    op = open('object.json')
+    data = json.load(op)
+    d = data['player'][0]
+
+    player = object_player.Player(d["name"],d["x"],d,["hp"],d["rad"],d["rotateSpeed"],d["fowardSpeed"],d["backSpeed"])
+
+    op.close()
+def exit():
+	pass
+
+def draw():
+    clear_canvas()
+    player.draw()
+    update_canvas()
+
+def update():
+    player.update()
+
+def handle_events():
+    global player,barrel
+    events = get_events()
+    for key in events:
+        if key.type == SDL_QUIT: gf.quit()
+        elif (key.type,key.key) == (SDL_KEYDOWN, SDLK_ESCAPE): gf.pop_state()
+        else:
+            player.handle_event(key)
+
+
+
+
+def pause():
+	pass
+
+def resume():
+	pass
+
+
+
+if __name__ == '__main__':
+    import sys
+    glCurrentModule = sys.modules[__name__]
+    open_canvas()
+    gf.run(glCurrentModule)
+    close_canvas()
